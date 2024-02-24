7795dd7a6829a1f9d1904961ec0222199e0e01bd
Heydi Garcia Sanchez <courantluna@gmail.com>
2024-02-24 17:37:05 -0400

    Using readTextFile

---
+++ src/Main.purs
@@ -0,0 +1,30 @@
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
+
+main :: Effect Unit
+main = do
+  log =<< readTextFile UTF8 "weather.dat"
+
+{-
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
+
+main :: Effect Unit
+main = do
+  log =<< readTextFile UTF8 "weather.dat"-}
+
+
+
+
